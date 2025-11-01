86075cf0eec40ecc543068c1dfdf385aab8267c5
nikitaelezarov <nikita.elezarov.01@mail.ru>
2025-11-01 14:03:50 +0300

    complete-task04-02

--- lesson-04/02.js
+++ lesson-04/02.js
@@ -15,4 +15,57 @@
 Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
 */
 
-function findUniqueElements() {}
+function findUniqueElements(array) {
+    let newArray = [];
+    for (i = 0; i < array.length; i++) {
+        // Проверяем, есть ли текущий элемент уже в новом массиве
+        let isUnique = true;
+        for (let j = 0; j < newArray.length; j++) {
+            if (array[i] === newArray[j]) {
+                isUnique = false;
+                break; // выходим из внутреннего цикла, если нашли дубликат
+            }
+        }
+        // Если элемент уникальный, добавляем его в новый массив
+        if (isUnique) {
+            newArray.push(array[i])
+        }
+    }
+    return newArray
+}
+
+console.log(findUniqueElements([1, 2, 3, 2, 1, 4]))//[1, 2, 3, 4]
+// function findUniqueElements(array) {
+//     let uniqueArray = [];
+//
+//     for (let i = 0; i < array.length; i++) {
+//         // Используем функцию includesElement для проверки наличия элемента
+//         if (!includesElement(uniqueArray, array[i])) {
+//             uniqueArray.push(array[i]);
+//         }
+//     }
+//
+//     return uniqueArray;
+// }
+
+
+// С помощью Set (более современный способ):
+// function findUniqueElements(array) {
+//     return Array.from(new Set(array));
+//     // или: return [...new Set(array)];
+// }
+// console.log(findUniqueElements([1, 2, 3, 2, 1, 4])); // [1, 2, 3, 4]
+
+// С помощью метода filter:
+// function findUniqueElements(array) {
+//     return array.filter((element, index) => array.indexOf(element) === index);
+// }
+// console.log(findUniqueElements([1, 2, 3, 2, 1, 4])); // [1, 2, 3, 4]
+
+// С помощью reduce:
+// function findUniqueElements(array) {
+//     return array.reduce((unique, item) => {
+//         return unique.includes(item) ? unique : [...unique, item];
+//     }, []);
+// }
+// console.log(findUniqueElements([1, 2, 3, 2, 1, 4])); // [1, 2, 3, 4]
