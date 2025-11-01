bab87283810c54f06bb2c4fbc7f5c220b38f3bad
nikitaelezarov <nikita.elezarov.01@mail.ru>
2025-11-01 17:46:32 +0300

    complete-task04-04

--- lesson-04/04.js
+++ lesson-04/04.js
@@ -12,4 +12,12 @@
 Пример работы функции: `doubleEachCharacter('hello')` вернёт `'hheelllloo'`
 */
 
-function doubleEachCharacter() {}
+function doubleEachCharacter(str) {
+    let newString = '';
+    for (let i = 0; i < str.length; i++) {
+        newString += str[i] + str[i];
+    }
+    return newString
+}
+
+console.log(doubleEachCharacter('hello'))//'hheelllloo'
