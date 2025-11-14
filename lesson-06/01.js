8eae307a83327890d8efa137bd3cb163cd76a294
nikitaelezarov <nikita.elezarov.01@mail.ru>
2025-11-14 20:02:17 +0300

    complete-task06-01

--- lesson-06/01.js
+++ lesson-06/01.js
@@ -22,20 +22,33 @@ const oddNumbers = filter(numbers, (element, index) => {
 
 console.log(oddNumbers) // Должен вывести: [1, 3, 5]
 */
-const filterArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
-const filtered = filterArray.filter((element,index) =>{
-    return element % 2 !== 0 && element >= 5;
-})
-console.log(filtered);
-
-
-// const filterArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
-// const filtered = [];
+// const filterArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+// const filtered = filterArray.filter((element,index) =>{
+//     return element % 2 !== 0 && element >= 5;
+// })
+// console.log(filtered);
 //
-// for (let i = 0; i < filterArray.length; i++) {
-//     if (filterArray[i] % 2 !== 0 && filterArray[i] >= 5) {
-//         filtered.push(filterArray[i]);
-//     }
-// }
 //
-// console.log(filtered); // [5, 7, 9, 11, 13, 15, 17, 19]
+// // const filterArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
+// // const filtered = [];
+// //
+// // for (let i = 0; i < filterArray.length; i++) {
+// //     if (filterArray[i] % 2 !== 0 && filterArray[i] >= 5) {
+// //         filtered.push(filterArray[i]);
+// //     }
+// // }
+// //
+// // console.log(filtered); // [5, 7, 9, 11, 13, 15, 17, 19]
+function filter(array, callback) {
+    const result = [];
+
+    for (let i = 0; i < array.length; i++) {
+        // Вызываем callback для каждого элемента
+        // Если callback возвращает true, добавляем элемент в результат
+        if (callback(array[i], i)) {
+            result.push(array[i]);
+        }
+    }
+
+    return result;
+}
