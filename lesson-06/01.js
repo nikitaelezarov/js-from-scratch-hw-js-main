d900a093aeed474204a9ca846d9cc55b65c2bc6f
nikitaelezarov <nikita.elezarov.01@mail.ru>
2025-11-14 19:56:32 +0300

    complete-task06-01

--- lesson-06/01.js
+++ lesson-06/01.js
@@ -22,5 +22,20 @@ const oddNumbers = filter(numbers, (element, index) => {
 
 console.log(oddNumbers) // Должен вывести: [1, 3, 5]
 */
+const filterArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+const filtered = filterArray.filter((element,index) =>{
+    return element % 2 !== 0 && element >= 5;
+})
+console.log(filtered);
 
-const filter = () => {}
+
+// const filterArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
+// const filtered = [];
+//
+// for (let i = 0; i < filterArray.length; i++) {
+//     if (filterArray[i] % 2 !== 0 && filterArray[i] >= 5) {
+//         filtered.push(filterArray[i]);
+//     }
+// }
+//
+// console.log(filtered); // [5, 7, 9, 11, 13, 15, 17, 19]
